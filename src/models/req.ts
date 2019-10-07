fb85a1ff86260ba1bb25661fa803d9548aef9781
kertibanaan1 <tigerhad@hotmail.com>
2019-10-07 20:58:28 +0700

    55

--- src/models/req.ts
+++ src/models/req.ts
@@ -3,13 +3,42 @@ import * as Knex from 'knex';
 export class ReqModel {
   dbName = 'employee';
 
-  getReq(db: Knex) {
+  getStock(db: Knex) {
     return db(this.dbName);
+  } 
+
+  showReq(db: Knex) {
+    return db('requisition')
+    .innerJoin ( 'cloth' , 'requisition.cId'  , 'cloth.cId')
+    .select(
+      'cloth.cId',
+      'cloth.cName',
+      'requisition.reqAmountCloth',
+      'requisition.status'
+    )
+    .where('requisition.status', '0')
+    .groupBy('cloth.cId');
+
   }
 
   insertReq(db:Knex, data) {
     return db('requisition')
     .insert(data);
+  }
+
+  updateReq(db: Knex, data) {
+    return db('requisition')
+      .update(data)
+      .where('cId',data.cId);
+      
+  }
+
+  delReq(db: Knex, cId: number , status: string) {
+    return db('requisition')
+      .del()
+      .where('cId', cId)
+      .andWhere('status', status);
+     
   }
 
 }
--- src/routes/req.ts
+++ src/routes/req.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express';
 import { Jwt } from '../models/jwt';
 import { ReqModel } from '../models/req';
 import * as HttpStatus from 'http-status-codes';
+import { stat } from 'fs';
 
 const jwt = new Jwt();
 
@@ -13,7 +14,23 @@ const reqModel = new ReqModel();
 router.get('/stock', async (req: Request, res: Response) => {
   let db = req.db;
   try {
-    const result = await reqModel.getReq(db);
+    const result = await reqModel.getStock(db);
+    res.send({ ok: true, statusCode: HttpStatus.OK, rows: result });
+  } catch (error) {
+    console.log(error.message);
+    res.send({
+      ok: false,
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: error.message
+    });
+  }
+});
+
+
+router.get('/showReq', async (req: Request, res: Response) => {
+  let db = req.db;
+  try {
+    const result = await reqModel.showReq(db);
     res.send({ ok: true, statusCode: HttpStatus.OK, rows: result });
   } catch (error) {
     console.log(error.message);
@@ -37,5 +54,34 @@ router.post('/insertReq', async (req: Request, res: Response) => {
         res.send({ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: err.message});
     }
 });
+
+router.post("/updateReg", async (req: Request, res: Response) => {
+  let db = req.db;
+  const data = req.body.data;
+ console.log('value', data);
+ 
+  try {
+      let result = await reqModel.updateReq(db, data);
+      res.send({ ok: true, statusCode: HttpStatus.OK, rows: result });
+  } catch (error) {
+      console.log(error.message);
+      res.send({
+          ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: error.message
+      });
+  }
+});
+
+router.post('/deleteReq', async (req: Request, res: Response) => {
+  let db = req.db;
+  const cId = req.body.cId;
+  const status = req.body.status;
+  try {
+      const result: any = await reqModel.delReq(db, cId, status);
+      res.send({ok: true, statusCode: HttpStatus.OK, rows: result});
+  } catch (err) {
+      res.send({ok: false, statusCode: HttpStatus.INTERNAL_SERVER_ERROR, message: err.message});
+  }
+});
+
 
 export default router;
